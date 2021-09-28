547a5a49da436abfa262be55949286ef335a305f
Sijia Wang <sijiaoh@outlook.com>
2021-09-28 10:41:31 +0900

    Add async await rules.

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,23 @@
  */
 
 import * as cfg from '../.eslintrc.json';
-module.exports = cfg;
+module.exports = {
+  ...cfg,
+  overrides: [
+    ...cfg.overrides,
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      rules: {
+        '@typescript-eslint/await-thenable': 'error',
+        'require-await': 'off',
+        '@typescript-eslint/require-await': 'error',
+        '@typescript-eslint/no-misused-promises': 'error',
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/promise-function-async': 'error',
+      },
+    },
+  ],
+};
